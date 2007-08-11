112399ecf28072f815e4cb0438a6bbab3fab7a76
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-08-11 23:21:25 +0000

    Format.
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@3811 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/container/src/java/org/picocontainer/ComponentAdapter.java
+++ pico/container/src/java/org/picocontainer/ComponentAdapter.java
@@ -7,22 +7,20 @@
  *****************************************************************************/
 package org.picocontainer;
 
-import org.picocontainer.behaviors.Cached;
-
 /**
- * A component adapter is responsible for providing a specific component instance. An instance of an implementation of
- * this interface is used inside a {@link PicoContainer} for every registered component or instance.  Each
- * <code>ComponentAdapter</code> instance has to have a key which is unique within that container. The key itself is
- * either a class type (normally an interface) or an identifier.
- *
+ * A component adapter is responsible for providing a specific component
+ * instance of type <T>. An instance of an implementation of this interface is
+ * used inside a {@link PicoContainer} for every registered component or
+ * instance. Each <code>ComponentAdapter</code> instance has to have a key
+ * which is unique within that container. The key itself is either a class type
+ * (normally an interface) or an identifier.
+ * 
  * @author Jon Tirs&eacute;n
  * @author Paul Hammant
  * @author Aslak Helles&oslash;y
- * @version $Revision$
- * @see MutablePicoContainer an extension of the PicoContainer interface which allows you to modify the contents of the
- *      container.
  */
 public interface ComponentAdapter<T> {
+
     /**
      * Retrieve the key associated with the component.
      *
@@ -41,7 +39,7 @@ public interface ComponentAdapter<T> {
 
     /**
      * Retrieve the component instance. This method will usually create a new instance each time it is called, but that
-     * is not required. For example, {@link Cached} will always return the
+     * is not required. For example, {@link org.picocontainer.behaviors.Cached} will always return the
      * same instance.
      *
      * @param container the {@link PicoContainer}, that is used to resolve any possible dependencies of the instance.
@@ -69,4 +67,5 @@ public interface ComponentAdapter<T> {
      * @param visitor the visitor.
      */
     void accept(PicoVisitor visitor);
+
 }
