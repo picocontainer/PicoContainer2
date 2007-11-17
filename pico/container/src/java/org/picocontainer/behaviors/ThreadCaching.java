ae253ee1e13e0423a7d85ac1ab3f73677ddc1438
proyal <proyal@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-11-17 23:54:44 +0000

    use generics
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4027 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/container/src/java/org/picocontainer/behaviors/ThreadCaching.java
+++ pico/container/src/java/org/picocontainer/behaviors/ThreadCaching.java
@@ -10,46 +10,56 @@
 
 package org.picocontainer.behaviors;
 
+import org.picocontainer.Characteristics;
 import org.picocontainer.ComponentAdapter;
+import org.picocontainer.ComponentMonitor;
+import org.picocontainer.LifecycleStrategy;
 import org.picocontainer.Parameter;
 import org.picocontainer.PicoCompositionException;
-import org.picocontainer.Characteristics;
-import org.picocontainer.ComponentMonitor;
-import org.picocontainer.behaviors.AbstractBehaviorFactory;
-import org.picocontainer.LifecycleStrategy;
 
 import java.util.Properties;
 
-/**
- * @author Paul Hammant
- */
+/** @author Paul Hammant */
 public class ThreadCaching extends AbstractBehaviorFactory {
 
-
-    public ComponentAdapter createComponentAdapter(ComponentMonitor componentMonitor, LifecycleStrategy lifecycleStrategy, Properties componentProperties, Object componentKey, Class componentImplementation, Parameter... parameters)
-            throws PicoCompositionException {
+    public <T> ComponentAdapter<T> createComponentAdapter(ComponentMonitor componentMonitor,
+                                                          LifecycleStrategy lifecycleStrategy,
+                                                          Properties componentProperties,
+                                                          Object componentKey,
+                                                          Class<T> componentImplementation,
+                                                          Parameter... parameters)
+        throws PicoCompositionException
+    {
         if (removePropertiesIfPresent(componentProperties, Characteristics.NO_CACHE)) {
             return super.createComponentAdapter(componentMonitor,
-                                                                             lifecycleStrategy,
-                                                                             componentProperties,
-                                                                             componentKey,
-                                                                             componentImplementation,
-                                                                             parameters);
+                                                lifecycleStrategy,
+                                                componentProperties,
+                                                componentKey,
+                                                componentImplementation,
+                                                parameters);
         }
         removePropertiesIfPresent(componentProperties, Characteristics.CACHE);
-        return new ThreadCached(super.createComponentAdapter(componentMonitor, lifecycleStrategy,
-                                                                componentProperties, componentKey, componentImplementation, parameters));
+        return new ThreadCached<T>(super.createComponentAdapter(componentMonitor,
+                                                                lifecycleStrategy,
+                                                                componentProperties,
+                                                                componentKey,
+                                                                componentImplementation,
+                                                                parameters));
 
     }
 
-    public ComponentAdapter addComponentAdapter(ComponentMonitor componentMonitor,
-                                    LifecycleStrategy lifecycleStrategy,
-                                    Properties componentProperties,
-                                    ComponentAdapter adapter) {
+    public <T> ComponentAdapter<T> addComponentAdapter(ComponentMonitor componentMonitor,
+                                                       LifecycleStrategy lifecycleStrategy,
+                                                       Properties componentProperties,
+                                                       ComponentAdapter<T> adapter)
+    {
         if (removePropertiesIfPresent(componentProperties, Characteristics.NO_CACHE)) {
             return super.addComponentAdapter(componentMonitor, lifecycleStrategy, componentProperties, adapter);
         }
         removePropertiesIfPresent(componentProperties, Characteristics.CACHE);
-        return new ThreadCached(super.addComponentAdapter(componentMonitor, lifecycleStrategy, componentProperties, adapter));
+        return new ThreadCached<T>(super.addComponentAdapter(componentMonitor,
+                                                             lifecycleStrategy,
+                                                             componentProperties,
+                                                             adapter));
     }
 }
