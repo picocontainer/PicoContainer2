280466887efecac23cc9a2bec94cdfa2f65e2f20
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-11-20 01:40:58 +0000

    a better example of Jasons need, and the false victory of am extended string key design
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4039 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/container/src/test/org/picocontainer/adapters/JasonsTestCase.java
+++ pico/container/src/test/org/picocontainer/adapters/JasonsTestCase.java
@@ -2,6 +2,10 @@ package org.picocontainer.adapters;
 
 import org.picocontainer.DefaultPicoContainer;
 import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.annotations.Inject;
+import org.picocontainer.injectors.AbstractInjector;
+import org.picocontainer.injectors.AnnotatedFieldInjection;
+import org.picocontainer.parameters.ComponentParameter;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -13,19 +17,45 @@ import junit.framework.TestCase;
 /** @author Paul Hammant */
 public class JasonsTestCase extends TestCase {
 
+    public void testNotQuiteJasonsNeed() {
+        MutablePicoContainer mpc = new DefaultPicoContainer(new AnnotatedFieldInjection());
+        // this level of verbosity is one way, not the way ....
+        mpc.addComponent(FruitBasket.class, FruitBasket.class, new ComponentParameter(Apple.class.getName()+ "one"),
+                         new ComponentParameter(Apple.class.getName()+ "two"), 
+                         new ComponentParameter(Apple.class.getName()+ "three"),
+                         new ComponentParameter(Apple.class.getName()+ "four"));
+        mpc.addComponent(Apple.class.getName()+ "one", AppleImpl1.class);
+        mpc.addComponent(Apple.class.getName()+ "two", AppleImpl2.class);
+        mpc.addComponent(Apple.class.getName()+ "three", AppleImpl3.class);
+        mpc.addComponent(Apple.class.getName()+ "four", AppleImpl4.class);
+        FruitBasket fb = mpc.getComponent(FruitBasket.class);
+        assertEquals(fb.one.getX(), 1);
+        assertEquals(fb.two.getX(), 2);
+        assertEquals(fb.three.getX(), 3);
+        assertEquals(fb.four.getX(), 4);
+    }
+
     public void testJasonsNeed() {
-        MutablePicoContainer mpc = new DefaultPicoContainer();
+        MutablePicoContainer mpc = new DefaultPicoContainer(new AnnotatedFieldInjection());
         mpc.addComponent(FruitBasket.class);
         mpc.addComponent(bindKey(Apple.class, "one"), AppleImpl1.class);
         mpc.addComponent(bindKey(Apple.class, "two"), AppleImpl2.class);
         mpc.addComponent(bindKey(Apple.class, "three"), AppleImpl3.class);
         mpc.addComponent(bindKey(Apple.class, "four"), AppleImpl4.class);
-        FruitBasket fb = mpc.getComponent(FruitBasket.class);
-//        assertEquals(fb.one.getX(), 1);
-//        assertEquals(fb.two.getX(), 2);
-//        assertEquals(fb.three.getX(), 3);
-//        assertEquals(fb.four.getX(), 4);
+        try {
+            // this level of terseness is the other way ....
+            // this should not be barfing if if we can get binding to annotations working
+            FruitBasket fb = mpc.getComponent(FruitBasket.class);
+            assertEquals(fb.one.getX(), 1);
+            assertEquals(fb.two.getX(), 2);
+            assertEquals(fb.three.getX(), 3);
+            assertEquals(fb.four.getX(), 4);
+            fail();
+        } catch (AbstractInjector.AmbiguousComponentResolutionException e) {
+            // which Apple to inject into which apple field ?
+        }
     }
+
 
     public interface Apple {
         int getX();
@@ -52,10 +82,17 @@ public class JasonsTestCase extends TestCase {
     }
 
     public static class FruitBasket {
+        @Inject
         private @Bind(id = "one") Apple one;
+        @Inject
         private @Bind(id = "two") Apple two;
+        @Inject
         private @Bind(id = "three") Apple three;
+        @Inject
         private @Bind(id = "four") Apple four;
+
+        public FruitBasket() {
+        }
     }
 
     // to become an annotation
