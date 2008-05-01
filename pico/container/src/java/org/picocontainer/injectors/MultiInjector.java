85f439e556ce91a5c06c9d61f548e06a8458cec1
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-05-01 00:45:36 +0000

    simpler
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4244 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/container/src/java/org/picocontainer/injectors/MultiInjector.java
+++ pico/container/src/java/org/picocontainer/injectors/MultiInjector.java
@@ -20,70 +20,20 @@ import java.lang.reflect.Type;
 import java.util.Set;
 
 /** @author Paul Hammant */
-public class MultiInjector extends AbstractInjector {
+public class MultiInjector extends CompositeInjector {
 
-    private final ConstructorInjector constuctorInjector;
-    private final SetterInjector setterInjector;
-    private final AnnotatedFieldInjector annotatedFieldInjector;
     private AnnotatedMethodInjector annotatedMethodInjector;
 
     public MultiInjector(Object componentKey,
                          Class componentImplementation,
                          Parameter[] parameters,
                          ComponentMonitor componentMonitor, LifecycleStrategy lifecycleStrategy, String setterPrefix, boolean useNames) {
-        super(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, useNames);
-        constuctorInjector = new ConstructorInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, useNames);
-        setterInjector = new SetterInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, setterPrefix, useNames) {
-            protected Object getOrMakeInstance(PicoContainer container,
-                                               Constructor constructor,
-                                               ComponentMonitor componentMonitor) {
-                return constuctorInjector.getComponentInstance(container);
-            }
+        super(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, useNames,
+                new ConstructorInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, useNames),
+                new SetterInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, setterPrefix, useNames),
+                new AnnotatedMethodInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, Inject.class, useNames),
+                new AnnotatedFieldInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, Inject.class, useNames));
 
-            protected Constructor getConstructor() {
-                return null;   
-            }
-
-        };
-        annotatedMethodInjector = new AnnotatedMethodInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, Inject.class, useNames) {
-            protected Object getOrMakeInstance(PicoContainer container,
-                                               Constructor constructor,
-                                               ComponentMonitor componentMonitor)  {
-                return setterInjector.getComponentInstance(container);
-            }
-
-            protected Constructor getConstructor() {
-                return null;
-            }
-
-        };
-        annotatedFieldInjector = new AnnotatedFieldInjector(componentKey, componentImplementation, parameters, componentMonitor, lifecycleStrategy, Inject.class, useNames) {
-            protected Object getOrMakeInstance(PicoContainer container,
-                                               Constructor constructor,
-                                               ComponentMonitor componentMonitor)  {
-                return annotatedMethodInjector.getComponentInstance(container);
-            }
-
-            protected Constructor getConstructor() {
-                return null;
-            }
-
-        };
-    }
-
-    public Object getComponentInstance(PicoContainer container, Type into) throws PicoCompositionException {
-        return annotatedFieldInjector.getComponentInstance(container, into);
-    }
-
-    public Object decorateComponentInstance(PicoContainer container, Type into, Object instance) {
-        return null; // not applicable
-    }
-
-    @Override
-    public void verify(PicoContainer container) throws PicoCompositionException {
-        constuctorInjector.verify(container);
-        constuctorInjector.verify(container);
-        annotatedMethodInjector.verify(container);
     }
 
     public String getDescriptor() {
