300e69f4e77d6c525d33f686acd7f7415530d3f3
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-05-17 09:35:50 +0000

    Added copyright for logging store, granted by Peter Donald to PicoContainer committers.
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4362 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/logging/src/java/org/picocontainer/logging/store/JDK14RawFormatter.java
+++ pico/logging/src/java/org/picocontainer/logging/store/JDK14RawFormatter.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) PicoContainer Organization. All rights reserved.            
+ * ------------------------------------------------------------------------- 
+ * The software in this package is published under the terms of the BSD      
+ * style license a copy of which has been included with this distribution in 
+ * the LICENSE.txt file.                                                     
+ */                                                                           
 package org.picocontainer.logging.store;
 
 import java.util.logging.Formatter;
@@ -7,7 +14,6 @@ import java.util.logging.LogRecord;
  * Class that extends base formatter to provide raw text output.
  *
  * @author Peter Donald
- * @version $Revision: 1.2 $ $Date: 2003/12/03 06:32:02 $
  */
 public class JDK14RawFormatter
     extends Formatter
