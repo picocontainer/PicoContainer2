4f4895cdb4070df69994ab6b117b15d437689dc1
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-05-30 14:55:47 +0000

    Moved remoting to sandbox
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4542 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- nanoextras/remoting/remoting/src/java/org/nanocontainer/remoting/rmi/KeyHolder.java
+++
@@ -1,9 +0,0 @@
-package org.nanocontainer.remoting.rmi;
-
-/**
- * @author Aslak Helles&oslash;y
- * @version $Revision$
- */
-public interface KeyHolder {
-    ByRefKey getKey();
-}
--- nanoextras/remoting/remoting/src/java/org/nanocontainer/remoting/rmi/NanoNaming.java
+++
@@ -1,8 +0,0 @@
-package org.nanocontainer.remoting.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface NanoNaming extends Remote {
-    Object lookup(ByRefKey key) throws RemoteException;
-}
