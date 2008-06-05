335e7882257a8bc8cb18fc026e759723a6a94f2b
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-06-05 16:59:42 +0000

    Removed Nano references.
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4581 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jsf-webapp/src/main/webapp/WEB-INF/nanocontainer.groovy
+++ web/examples/jsf-webapp/src/main/webapp/WEB-INF/nanocontainer.groovy
@@ -1,9 +1,9 @@
 pico = builder.container(parent: parent) {
 	if(assemblyScope instanceof javax.servlet.ServletContext) {
-		component(key: 'org.nanocontainer.nanowar.sample.dao.CheeseDao', class: 'org.nanocontainer.nanowar.sample.dao.simple.MemoryCheeseDao')
-		component(key: 'org.nanocontainer.nanowar.sample.service.CheeseService', class: 'org.nanocontainer.nanowar.sample.service.defaults.DefaultCheeseService')
+		component(key: 'org.picocontainer.web.sample.dao.CheeseDao', class: 'org.picocontainer.web.sample.dao.simple.MemoryCheeseDao')
+		component(key: 'org.picocontainer.web.sample.service.CheeseService', class: 'org.picocontainer.web.sample.service.defaults.DefaultCheeseService')
 	} else if (assemblyScope instanceof javax.servlet.ServletRequest) {
-		component(key: 'cheeseBean', class: 'org.nanocontainer.nanowar.samples.jsf.ListCheeseController')
-		component(key: 'addCheeseBean', class: 'org.nanocontainer.nanowar.samples.jsf.AddCheeseController')
+		component(key: 'cheeseBean', class: 'org.picocontainer.web.samples.jsf.ListCheeseController')
+		component(key: 'addCheeseBean', class: 'org.picocontainer.web.samples.jsf.AddCheeseController')
 	}
 }
