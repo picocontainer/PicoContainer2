28bda48495c5fa45d996c4c09ba7ccea2596acac
rimovm <rimovm@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-06-17 20:18:58 +0000

    Backwards Compatibility stub as discussed on list.
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4665 ac66bb80-72f5-0310-8d68-9f556cfffb23

---
+++ pico/container/src/java/org/picocontainer/containers/CommandLineArgumentsPicoContainer.java
@@ -0,0 +1,102 @@
+/*****************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.picocontainer.containers;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.picocontainer.PicoContainer;
+
+/**
+ * Compatibility stub. This class was renamed to CommandLinePicoContainer for
+ * PicoContainer version 2.4.
+ * 
+ * @author Michael Rimov
+ * @deprecated Use {@link org.picocontainer.containers.CommandLinePicoContainer}
+ *             instead.
+ */
+@Deprecated
+public final class CommandLineArgumentsPicoContainer extends
+		CommandLinePicoContainer {
+
+	/**
+	 * Serialization UUID.
+	 */
+	private static final long serialVersionUID = 5791801617108577150L;
+
+	/**
+	 * @param separator
+	 * @param arguments
+	 */
+	public CommandLineArgumentsPicoContainer(final String separator,
+			final String[] arguments) {
+		super(separator, arguments);
+	}
+
+	/**
+	 * @param separator
+	 * @param arguments
+	 * @param parent
+	 */
+	public CommandLineArgumentsPicoContainer(final String separator,
+			final String[] arguments, final PicoContainer parent) {
+		super(separator, arguments, parent);
+	}
+
+	/**
+	 * @param separator
+	 * @param argumentsProps
+	 * @throws IOException
+	 */
+	public CommandLineArgumentsPicoContainer(final String separator,
+			final StringReader argumentsProps) throws IOException {
+		super(separator, argumentsProps);
+	}
+
+	/**
+	 * @param separator
+	 * @param argumentProperties
+	 * @param arguments
+	 * @throws IOException
+	 */
+	public CommandLineArgumentsPicoContainer(final String separator,
+			final StringReader argumentProperties, final String[] arguments)
+			throws IOException {
+		super(separator, argumentProperties, arguments);
+	}
+
+	/**
+	 * @param separator
+	 * @param argumentProperties
+	 * @param arguments
+	 * @param parent
+	 * @throws IOException
+	 */
+	public CommandLineArgumentsPicoContainer(final String separator,
+			final StringReader argumentProperties, final String[] arguments,
+			final PicoContainer parent) throws IOException {
+		super(separator, argumentProperties, arguments, parent);
+	}
+
+	/**
+	 * @param arguments
+	 */
+	public CommandLineArgumentsPicoContainer(final String[] arguments) {
+		super(arguments);
+	}
+
+	/**
+	 * @param arguments
+	 * @param parent
+	 */
+	public CommandLineArgumentsPicoContainer(final String[] arguments,
+			final PicoContainer parent) {
+		super(arguments, parent);
+	}
+}
