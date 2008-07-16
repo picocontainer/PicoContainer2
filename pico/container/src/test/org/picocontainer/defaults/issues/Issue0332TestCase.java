d134b9ade77bcada99dd634bab6be818869a0370
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-07-16 15:11:20 +0000

    Mike failing testcase
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4843 ac66bb80-72f5-0310-8d68-9f556cfffb23

---
+++ pico/container/src/test/org/picocontainer/defaults/issues/Issue0332TestCase.java
@@ -0,0 +1,63 @@
+package org.picocontainer.defaults.issues;
+
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.behaviors.Caching;
+import org.picocontainer.parameters.ComponentParameter;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertNotNull;import static junit.framework.Assert.assertTrue;
+
+public class Issue0332TestCase {
+
+    /**
+     * Sample class that demonstrates literal collection handling.
+     */
+    static class Searcher {
+    	private final List<String> searchPath;
+
+    	public Searcher(List<String> searchPath) {
+    		this.searchPath = searchPath;
+    	}
+
+    	public List<String> getSearchPath() {
+    		return searchPath;
+    	}
+    }
+    
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+
+    //@Test
+    public void canInstantiateAutowiredCollectionThatAreDefinedExplicitly() {
+    	MutablePicoContainer pico = new DefaultPicoContainer(new Caching());
+    	List<String> searchPath = new ArrayList<String>();
+    	searchPath.add("a");
+    	searchPath.add("b");
+
+    	pico.addComponent("searchPath",searchPath)
+    		.addComponent(Searcher.class);
+
+    	assertNotNull(pico.getComponent(Searcher.class));
+    	assertNotNull(pico.getComponent(Searcher.class).getSearchPath());
+    }
+
+    //@Test 
+    public void canInstantiateExplicitCollectionWithComponentParameter() {
+    	MutablePicoContainer pico = new DefaultPicoContainer(new Caching());
+    	List<String> searchPath = new ArrayList<String>();
+    	searchPath.add("a");
+    	searchPath.add("b");
+
+    	pico.addComponent("searchPath",searchPath)
+    		.addComponent(Searcher.class, Searcher.class, new ComponentParameter("searchPath"));
+
+    	assertNotNull(pico.getComponent(Searcher.class));
+    	assertNotNull(pico.getComponent(Searcher.class).getSearchPath());
+    }
+}
