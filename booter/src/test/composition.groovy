db357775081bc719aad6399487ace495858bab9b
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-08-18 04:53:14 +0000

    getting it workin again
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4903 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- booter/src/test/composition.groovy
+++ booter/src/test/composition.groovy
@@ -1,15 +1,13 @@
-import org.nanocontainer.reflection.ImplementationHidingNanoPicoContainer
-
-pico = builder.container(parent:parent, class:ImplementationHidingNanoPicoContainer) {
+pico = builder.container(parent:parent) {
     classPathElement(path:"test-comps/api.jar")
     classLoader {
         classPathElement(path:"test-comps/honeyimpl.jar")
-        component(classNameKey:"org.nanocontainer.booter.Honey", class:"org.nanocontainer.booter.BeeHiveHoney")
+        component(classNameKey:"org.picocontainer.booter.Honey", class:"org.picocontainer.booter.BeeHiveHoney")
     }
     classLoader {
         classPathElement(path:"test-comps/bearimpl.jar") {
              grant(new java.net.SocketPermission("yahoo.com:80", "connect"))
         }
-        component(class:"org.nanocontainer.booter.BrownBear")
+        component(class:"org.picocontainer.booter.BrownBear")
     }
 }
