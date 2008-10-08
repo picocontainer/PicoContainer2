c2c8d838f266cb1e40f99763b42ff7815ae7af6f
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-10-08 16:05:08 +0000

    don't put it in the request store
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4928 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/struts2-webapp/src/java/org/picocontainer/web/sample/struts2/BrandingPicoServletFilter.java
+++ web/examples/struts2-webapp/src/java/org/picocontainer/web/sample/struts2/BrandingPicoServletFilter.java
@@ -7,6 +7,7 @@ import org.picocontainer.ComponentAdapter;
 import org.picocontainer.PicoContainer;
 import org.picocontainer.PicoCompositionException;
 import org.picocontainer.PicoVisitor;
+import org.picocontainer.Characteristics;
 import org.picocontainer.adapters.AbstractAdapter;
 
 import javax.servlet.ServletException;
@@ -19,7 +20,8 @@ import java.io.IOException;
 public class BrandingPicoServletFilter extends PicoObjectFactory.ServletFilter {
 
     protected void initAdditionalScopedComponents(MutablePicoContainer sessionContainer, MutablePicoContainer reqContainer) {
-        reqContainer.addAdapter(new BrandFromDomainAdapter());
+        reqContainer.as(Characteristics.NO_CACHE).addAdapter(new BrandFromDomainAdapter());
+        System.out.println("-->" + reqContainer.getComponentAdapter(Brand.class));
     }
 
     private static ThreadLocal<ServletRequest> request = new ThreadLocal<ServletRequest>();
