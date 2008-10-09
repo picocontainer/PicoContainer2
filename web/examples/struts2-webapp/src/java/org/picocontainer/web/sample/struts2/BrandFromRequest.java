07b59e3c97ca7eef684340ec9fdb63239fc767ba
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-10-09 13:18:52 +0000

    short use of request
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@4931 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/struts2-webapp/src/java/org/picocontainer/web/sample/struts2/BrandFromRequest.java
+++ web/examples/struts2-webapp/src/java/org/picocontainer/web/sample/struts2/BrandFromRequest.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.
+ * ---------------------------------------------------------------------------
+ * The software in this package is published under the terms of the BSD style
+ * license a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ ******************************************************************************/
 package org.picocontainer.web.sample.struts2;
 
 import org.picocontainer.web.sample.service.Brand;
@@ -6,13 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 
 public class BrandFromRequest implements Brand {
 
-    private final HttpServletRequest req;
+    private final String name;
 
     public BrandFromRequest(HttpServletRequest req) {
-        this.req = req;
+        this.name = req.getRemoteHost().toUpperCase();
     }
 
     public String getName() {
-        return req.getRemoteHost().toUpperCase();
+        return name;
     }
 }
