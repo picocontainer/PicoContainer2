0780d543f403a68686545f37a935c8c5d7e75973
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-11-09 13:20:08 +0000

    selenium test doing something (just a start)
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5048 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/SeleniumTest.java
+++ web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/SeleniumTest.java
@@ -9,23 +9,16 @@ public class SeleniumTest {
     protected DefaultSelenium createSeleniumClient(String url) throws Exception {
         return new DefaultSelenium("localhost", 4444, "*firefox", url);
     }
-
+    
     @Test
-    public void dummy() {
-        assertTrue(true);
-    }
-
-    //@Test
-    public void testSomethingSimple() throws Exception {
+    public void applicationOpensToCorrectStartPage() throws Exception {
         DefaultSelenium selenium = createSeleniumClient("http://localhost:8080/");
 
         selenium.start();
 
         selenium.open("/remoting-jqueryemail-webapp/index.html");
 
-        Thread.sleep(1000 * 1000);
-
-        assertEquals("Geronimo Console", selenium.getTitle());
+        assertEquals("jQuery Message Demo", selenium.getTitle());
 
         selenium.stop();
     }
