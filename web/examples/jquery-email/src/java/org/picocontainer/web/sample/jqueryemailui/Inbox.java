3088c0442bb5d828ba64cab244681bc511fa95eb
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-11-18 22:26:24 +0000

    simplify
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5067 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/java/org/picocontainer/web/sample/jqueryemailui/Inbox.java
+++ web/examples/jquery-email/src/java/org/picocontainer/web/sample/jqueryemailui/Inbox.java
@@ -1,17 +1,9 @@
 package org.picocontainer.web.sample.jqueryemailui;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class Inbox extends Mailbox {
 
     public Inbox(MessageStore store, User user) {
-        super(foo(store, user));
-    }
-
-    private static Map<Integer, MessageData> foo(MessageStore store, User user) {
-        return store.inboxFor(user.getName());
+        super(store.inboxFor(user.getName()));
     }
 
 }
