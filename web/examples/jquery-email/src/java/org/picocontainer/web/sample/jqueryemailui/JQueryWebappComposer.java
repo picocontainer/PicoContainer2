98f712e76dedba880ba0da49c3b915631f86c87d
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-11-18 23:50:24 +0000

    better static helper method
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5069 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/java/org/picocontainer/web/sample/jqueryemailui/JQueryWebappComposer.java
+++ web/examples/jquery-email/src/java/org/picocontainer/web/sample/jqueryemailui/JQueryWebappComposer.java
@@ -1,10 +1,10 @@
 package org.picocontainer.web.sample.jqueryemailui;
 
 import org.picocontainer.web.WebappComposer;
-import org.picocontainer.web.StringFromRequest;
+import static org.picocontainer.web.StringFromRequest.addStringAdapters;
+import static org.picocontainer.web.IntFromRequest.addIntAdapters;
 import org.picocontainer.web.IntFromRequest;
 import org.picocontainer.MutablePicoContainer;
-import org.picocontainer.Characteristics;
 import static org.picocontainer.Characteristics.USE_NAMES;
 
 public class JQueryWebappComposer implements WebappComposer {
@@ -18,18 +18,13 @@ public class JQueryWebappComposer implements WebappComposer {
     }
 
     public void composeRequest(MutablePicoContainer container) {
-        container.addAdapter(new StringFromRequest("to"));
-        container.addAdapter(new StringFromRequest("subject"));
-        container.addAdapter(new StringFromRequest("message"));
-        container.addAdapter(new IntFromRequest("msgId"));
-        container.addAdapter(new StringFromRequest("view"));
-        container.addAdapter(new StringFromRequest("userName"));
-        container.addAdapter(new StringFromRequest("password"));
-        container.addAdapter(new IntFromRequest("userId"));
+        addStringAdapters(container, "to", "subject", "message", "view", "userName", "password", "userId");
+        addIntAdapters(container, "msgId");
         container.addAdapter(new User.FromCookie());
         container.as(USE_NAMES).addComponent(Auth.class);
         container.as(USE_NAMES).addComponent(Inbox.class);
         container.as(USE_NAMES).addComponent(Sent.class);
     }
+
 
 }
