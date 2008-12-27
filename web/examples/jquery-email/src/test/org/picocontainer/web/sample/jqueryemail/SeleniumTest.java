67c82f40613575b789164210011ce61b270c609a
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-12-27 20:31:17 +0000

    small work on the selenium test
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5117 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/SeleniumTest.java
+++ web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/SeleniumTest.java
@@ -1,24 +1,35 @@
 package org.picocontainer.web.sample.jqueryemail;
 
 import com.thoughtworks.selenium.DefaultSelenium;
-import static junit.framework.Assert.assertEquals;import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 import org.junit.Test;
 
 public class SeleniumTest {
 
-    protected DefaultSelenium createSeleniumClient(String url) throws Exception {
-        return new DefaultSelenium("localhost", 4444, "*firefox", url);
-    }
-    
     @Test
     public void applicationOpensToCorrectStartPage() throws Exception {
-        DefaultSelenium selenium = createSeleniumClient("http://localhost:8080/");
+
+        DefaultSelenium selenium = new DefaultSelenium("localhost", 4444,
+                "*firefox", "http://localhost:8080/");
 
         selenium.start();
 
         selenium.open("/remoting-jqueryemail-webapp/index.html");
 
         assertEquals("Pico Web Remoting and jQuery Message Sample App", selenium.getTitle());
+
+        //assertTrue(selenium.isTextPresent("Login Please"));
+
+        selenium.type("userName", "Gil Bates");
+        selenium.type("password", "1234");
+        selenium.click("submitLogin");
+
+        // unfortunately, the version of Selenium linked to the Maven plugin does
+        // mot contain the new waitFor/condition stuff.
+        Thread.sleep(1 * 1000);
+
+        assertTrue(selenium.isTextPresent("Nice Example")); // from first message
 
         selenium.stop();
     }
