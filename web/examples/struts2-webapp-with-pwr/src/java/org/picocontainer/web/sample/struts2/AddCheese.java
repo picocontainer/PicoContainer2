acac6db44f8af48b40ab8c1a410471869f65525c
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-12-29 07:32:49 +0000

    demo of Struts2 and PWR together
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5128 ac66bb80-72f5-0310-8d68-9f556cfffb23

---
+++ web/examples/struts2-webapp-with-pwr/src/java/org/picocontainer/web/sample/struts2/AddCheese.java
@@ -0,0 +1,34 @@
+package org.picocontainer.web.sample.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import org.picocontainer.web.sample.model.Cheese;
+import org.picocontainer.web.sample.model.Brand;
+import org.picocontainer.web.sample.service.CheeseService;
+
+public class AddCheese extends ActionSupport {
+
+    private Cheese cheese = new Cheese();
+    private CheeseService cheeseService;
+    private final Brand brand;
+
+    public AddCheese(CheeseService cheeseService, Brand brand) {
+        this.cheeseService = cheeseService;
+        this.brand = brand;
+    }
+
+    public Cheese getCheese() {
+        return cheese;
+    }
+
+    public String getBrand() {
+        return "Brand:" + brand;
+    }
+
+    public String execute() throws Exception {
+        System.out.println("Adding cheese "+cheese + " for " + brand.getName());
+        cheeseService.save(cheese);
+        return SUCCESS;
+    }
+
+}
