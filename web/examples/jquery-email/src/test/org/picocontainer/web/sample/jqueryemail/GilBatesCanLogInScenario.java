291111dfd8a1e56ab2236d3bed859cea87303d25
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-01-21 22:49:46 +0000

    nearer to getting jbehave running
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5176 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/GilBatesCanLogInScenario.java
+++ web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/GilBatesCanLogInScenario.java
@@ -2,17 +2,15 @@ package org.picocontainer.web.sample.jqueryemail;
 
 import org.jbehave.scenario.JUnitScenario;
 import org.jbehave.scenario.PropertyBasedConfiguration;
-import org.jbehave.scenario.steps.CandidateSteps;
 import org.jbehave.scenario.parser.PatternScenarioParser;
 import org.jbehave.scenario.parser.ClasspathScenarioDefiner;
 import org.jbehave.scenario.parser.UnderscoredCamelCaseResolver;
-import org.junit.Test;
 import org.junit.Before;
 import com.thoughtworks.selenium.Selenium;
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.condition.JUnitConditionRunner;
 
-public class GilBatesCanLogIn extends JUnitScenario {
+public class GilBatesCanLogInScenario extends JUnitScenario {
 
     private Selenium selenium;
 
@@ -25,11 +23,11 @@ public class GilBatesCanLogIn extends JUnitScenario {
 
     }
 
-    public GilBatesCanLogIn() {
+    public GilBatesCanLogInScenario() {
         this(Thread.currentThread().getContextClassLoader());
     }
 
-    public GilBatesCanLogIn(final ClassLoader classLoader) {
+    public GilBatesCanLogInScenario(final ClassLoader classLoader) {
         super(new PropertyBasedConfiguration() {
             @Override
             public ClasspathScenarioDefiner forDefiningScenarios() {
