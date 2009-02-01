10e605e8938b23244efb7407819c8addab3e3972
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-02-01 15:29:21 +0000

    add ww2 line
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5232 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/copywarfiles.sh
+++ web/copywarfiles.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cp examples/webwork-webapp/target/webwork-webapp.war distribution/src/site/resources/warfiles/pico-webwork1-demo.war
+cp examples/webwork2-webapp/target/webwork2-webapp.war distribution/src/site/resources/warfiles/pico-webwork2-demo.war
