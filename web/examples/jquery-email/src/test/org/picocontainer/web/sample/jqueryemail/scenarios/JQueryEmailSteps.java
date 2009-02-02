29063f427cc2633a50f452d69b5ca337aa346d16
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-02-02 10:22:03 +0000

    Renamed method params.
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5241 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/scenarios/JQueryEmailSteps.java
+++ web/examples/jquery-email/src/test/org/picocontainer/web/sample/jqueryemail/scenarios/JQueryEmailSteps.java
@@ -20,91 +20,83 @@ import com.thoughtworks.selenium.condition.Not;
 import com.thoughtworks.selenium.condition.Presence;
 import com.thoughtworks.selenium.condition.Text;
 
-
 public class JQueryEmailSteps extends Steps {
 
 	private final Selenium selenium;
-    private final ConditionRunner runner;
-    private static final StepsConfiguration configuration = new StepsConfiguration();
+	private final ConditionRunner runner;
+	private static final StepsConfiguration configuration = new StepsConfiguration();
 	private static final boolean DEBUG = false;
-    
-    public JQueryEmailSteps() {
-    	super(configuration);
-    	if ( DEBUG ){
-    		configuration.useMonitor(new PrintStreamStepMonitor());
-    	}
-        this.selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080");
-        this.runner =  new JUnitConditionRunner(selenium, 10, 100, 1000);
-    }
 
-    @BeforeScenario
-    public void setUp() throws Exception {
-        selenium.start();
+	public JQueryEmailSteps() {
+		super(configuration);
+		if (DEBUG) {
+			configuration.useMonitor(new PrintStreamStepMonitor());
+		}
+		this.selenium = new DefaultSelenium("localhost", 4444, "*firefox",
+				"http://localhost:8080");
+		this.runner = new JUnitConditionRunner(selenium, 10, 100, 1000);
+	}
 
-    }
+	@BeforeScenario
+	public void setUp() throws Exception {
+		selenium.start();
 
-    @AfterScenario
-    public void tearDown() throws Exception {
-        selenium.close();
-        selenium.stop();
-    }
+	}
 
-    @Given("nobody is logged in")
-    public void nobodyLoggedIn() {
-        selenium.open("/remoting-jqueryemail-webapp/");
-        try {
-            selenium.click("link=Log Out");
-        } catch (Exception e) {
-        }
-    }
+	@AfterScenario
+	public void tearDown() throws Exception {
+		selenium.close();
+		selenium.stop();
+	}
 
-    @When("user $user with password $password logs in")
-    public void logIn(String user, String pw) {
-        waitFor(new Presence("id=userName"));
-        selenium.type("id=userName", user);
-        selenium.type("id=password", pw);
-        selenium.click("id=submitLogin");
-    }
+	@Given("nobody is logged in")
+	public void nobodyLoggedIn() {
+		selenium.open("/remoting-jqueryemail-webapp/");
+		try {
+			selenium.click("link=Log Out");
+		} catch (Exception e) {
+		}
+	}
 
-    @Then("the Inbox should be visible")
-    public void inBoxIsVisible() {
-    	textIsVisible("Instant Millionaire");
-    }
+	@When("user $userName with password $password logs in")
+	public void logIn(String userName, String password) {
+		waitFor(new Presence("id=userName"));
+		selenium.type("id=userName", userName);
+		selenium.type("id=password", password);
+		selenium.click("id=submitLogin");
+	}
 
-    @Then("the Inbox should not be visible")
-    public void inBoxIsNotVisible() {
-    	textIsNotVisible("Instant Millionaire");
-    }
+	@Then("the Inbox should be visible")
+	public void inBoxIsVisible() {
+		textIsVisible("Instant Millionaire");
+	}
 
-    @Then("the text \"$text\" should be visible")
-    public void textIsVisible(String text) {
-        waitFor(new Text(text));
-    }
+	@Then("the Inbox should not be visible")
+	public void inBoxIsNotVisible() {
+		textIsNotVisible("Instant Millionaire");
+	}
 
-    @Then("the text \"$text\" should not be visible")
-    public void textIsNotVisible(String text) {
-        waitFor(new Not(new Text(text)));
-    }
+	@Then("the text \"$text\" should be visible")
+	public void textIsVisible(String text) {
+		waitFor(new Text(text));
+	}
 
-    private void waitFor(Condition condition) {
-        runner.waitFor(condition);
-        waitFor(1);
-    }
+	@Then("the text \"$text\" should not be visible")
+	public void textIsNotVisible(String text) {
+		waitFor(new Not(new Text(text)));
+	}
 
-    private void waitFor(int seconds) {
-    	try {
+	private void waitFor(Condition condition) {
+		runner.waitFor(condition);
+		waitFor(1);
+	}
+
+	private void waitFor(int seconds) {
+		try {
 			TimeUnit.SECONDS.sleep(seconds);
 		} catch (InterruptedException e) {
 			// continue
 		}
-    }
+	}
 
 }
-
-
-
-
-
-
-
-
