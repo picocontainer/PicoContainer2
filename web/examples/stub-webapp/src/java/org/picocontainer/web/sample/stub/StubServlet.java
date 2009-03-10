8ed5629b0801fce7417faa5f6fbc6065f2d4d80a
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-03-10 17:05:46 +0000

    chg to html info
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5306 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/stub-webapp/src/java/org/picocontainer/web/sample/stub/StubServlet.java
+++ web/examples/stub-webapp/src/java/org/picocontainer/web/sample/stub/StubServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class StubServlet extends HttpServlet {
 
@@ -19,6 +20,9 @@ public class StubServlet extends HttpServlet {
         protected void setAppContainer(MutablePicoContainer container) {
         }
         protected void setRequestContainer(MutablePicoContainer container) {
+            if (currentRequestContainer == null) {
+                currentRequestContainer = new ThreadLocal<MutablePicoContainer>();
+            }
             currentRequestContainer.set(container);
         }
         protected void setSessionContainer(MutablePicoContainer container) {
@@ -26,13 +30,20 @@ public class StubServlet extends HttpServlet {
     }
 
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        MutablePicoContainer container = currentRequestContainer.get();
-        RequestScopeComp requestScopeComp = container.getComponent(RequestScopeComp.class);
-
+        Logger logger = Logger.getLogger(this.getClass().getName());
+        logger.info("s0");
         ServletOutputStream os = response.getOutputStream();
         os.print("<html><body><p>");
-        os.println(requestScopeComp.getCounter());
+        try {
+            response.setContentType("text/html");
+            MutablePicoContainer container = currentRequestContainer.get();
+            RequestScopeComp requestScopeComp = container.getComponent(RequestScopeComp.class);
+
+            os.println(requestScopeComp.getCounter());
+        } catch (Throwable e) {
+            logger.info("s1:"+e.getMessage());
+            logger.info("s2:"+e.getClass().getName());
+        }
         os.print("</p></body></html>");
     }
 }
