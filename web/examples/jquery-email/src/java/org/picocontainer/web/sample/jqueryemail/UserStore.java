d4121d39e473a70b92a425a0c229e62a03c33f45
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-03-29 02:00:22 +0000

    store query for userStore access
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5327 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/jquery-email/src/java/org/picocontainer/web/sample/jqueryemail/UserStore.java
+++ web/examples/jquery-email/src/java/org/picocontainer/web/sample/jqueryemail/UserStore.java
@@ -8,20 +8,25 @@ import java.util.logging.Logger;
 public class UserStore {
 
     private PersistenceManager pm;
+    private final QueryStore queryStore;
 
-    public UserStore(PersistenceManager pm) {
+    public UserStore(PersistenceManager pm, QueryStore queryStore) {
         this.pm = pm;
+        this.queryStore = queryStore;
     }
 
     public User getUser(String name) {
-        Query query = pm.newQuery("javax.jdo.query.JDOQL", "SELECT FROM " + User.class.getName() + " WHERE name == \"" + name + "\"");
-//        query.declareImports("import java.lang.String");
-//        query.declareParameters("String name");
-        Collection<User> messages = (Collection<User>) query.execute();
+        Query query = queryStore.get("GU");
+        if (query == null) {
+            query = pm.newQuery(User.class.getName(), "name == user_name");
+            query.declareImports("import java.lang.String");
+            query.declareParameters("String user_name");
+            queryStore.put("GU", query);
+        }
+        Collection<User> messages = (Collection<User>) query.execute(name);
         if (messages != null && messages.size() > 0) {
             return messages.iterator().next();
         } else {
-            Logger.getAnonymousLogger().info("for [" + name + "]");
             return null;
         }
     }
