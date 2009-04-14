c39e53f91c9c06cb876157697f42a4d125fd0323
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-04-14 20:58:44 +0000

    webwork2 works with GAE
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5379 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/web-webwork2/src/java/org/picocontainer/web/webwork2/WebWork2PicoServletContainerListener.java
+++ web/web-webwork2/src/java/org/picocontainer/web/webwork2/WebWork2PicoServletContainerListener.java
@@ -5,11 +5,13 @@ import org.picocontainer.web.PicoServletContainerListener;
 import javax.servlet.ServletContextEvent;
 
 import com.opensymphony.xwork.ObjectFactory;
+import ognl.OgnlRuntime;
 
 @SuppressWarnings("serial")
 public class WebWork2PicoServletContainerListener extends PicoServletContainerListener {
 
     public void contextInitialized(ServletContextEvent event) {
+        OgnlRuntime.setSecurityManager(null);
         super.contextInitialized(event);
         ObjectFactory.setObjectFactory(new PicoObjectFactory());
     }
