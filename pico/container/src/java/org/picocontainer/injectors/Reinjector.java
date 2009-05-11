e9d49c08607e2b4c70da81aa8c47a179dd271814
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-05-11 10:00:23 +0000

    some comments
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5391 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/container/src/java/org/picocontainer/injectors/Reinjector.java
+++ pico/container/src/java/org/picocontainer/injectors/Reinjector.java
@@ -57,22 +57,29 @@ public class Reinjector {
      * @return the result of the reinjection-method invocation.
      */
     public Object reinject(Class<?> key, Method reinjectionMethod) {
-        return reinject(key, key, parent.getComponent(key), new MethodInjection(reinjectionMethod));
+        return reinject(key, key, parent.getComponent(key), NO_PROPERTIES, new MethodInjection(reinjectionMethod));
+    }
+
+    /**
+     * Reinjecting into a method.
+     * @param key the component-key from the parent set of components to inject into (key and impl are the same)
+     * @param reinjectionFactory the InjectionFactory to use for reinjection.
+     * @return the result of the reinjection-method invocation.
+     */
+    public Object reinject(Class<?> key, InjectionFactory reinjectionFactory) {
+        Object o = reinject(key, key, parent.getComponent(key), NO_PROPERTIES, reinjectionFactory);
+        return o;
     }
 
     /**
      * Reinjecting into a method.
      * @param key the component-key from the parent set of components to inject into
+     * @param implementation the implementation of the component that is going to result.
      * @param reinjectionFactory the InjectionFactory to use for reinjection.
-     * @return the result of the reinjection-method invocation.
+     * @return
      */
-    public Object reinject(Class<?> key, InjectionFactory reinjectionFactory) {
-        Object o = reinject(key, key, parent.getComponent(key), reinjectionFactory);
-        return o;
-    }
-
     public Object reinject(Class<?> key, Class<?> impl, InjectionFactory reinjectionFactory) {
-        return reinject(key, impl, parent.getComponent(key), reinjectionFactory);
+        return reinject(key, impl, parent.getComponent(key), NO_PROPERTIES, reinjectionFactory);
     }
 
     /**
@@ -84,16 +91,15 @@ public class Reinjector {
      * @return the result of the reinjection-method invocation.
      */
     public Object reinject(Class<?> key, Class implementation, Object instance, InjectionFactory reinjectionFactory) {
-        Object o = reinject(key, implementation, instance, NO_PROPERTIES, reinjectionFactory);
-        return o;
+        return reinject(key, implementation, instance, NO_PROPERTIES, reinjectionFactory);
     }
 
     /**
-     *
+     * Reinjecting into a method.
      * @param key the component-key from the parent set of components to inject into
      * @param implementation the implementation of the component that is going to result.
      * @param instance the object that has the provider method to be invoked
-     * @param properties
+     * @param properties for reinjection
      * @param reinjectionFactory the InjectionFactory to use for reinjection.
      * @return the result of the reinjection-method invocation.
      */
@@ -102,8 +108,7 @@ public class Reinjector {
         Reinjection reinjection = new Reinjection(reinjectionFactory, parent);
         org.picocontainer.Injector injector = (org.picocontainer.Injector) reinjection.createComponentAdapter(
                 monitor, NO_LIFECYCLE, properties, key, implementation, null);
-        Object o = injector.decorateComponentInstance(parent, null, instance);
-        return o;
+        return injector.decorateComponentInstance(parent, null, instance);
     }
 
 }
