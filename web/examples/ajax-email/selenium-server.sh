7153b74f2617cf659b1d64bb4cf5c184fcdeab03
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-08-30 00:53:44 +0000

    Made M2_REPOSITORY configurable
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5511 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- web/examples/ajax-email/selenium-server.sh
+++ web/examples/ajax-email/selenium-server.sh
@@ -8,12 +8,16 @@ start()
 
 # ----- Execute the commands -----------------------------------------
 
-VERSION=$1
+M2_REPOSITORY=$1
+VERSION=$2
+if [ "$M2_REPOSITORY" == "" ]; then
+M2_REPOSITORY="$HOME/.m2/repository"
+fi
 if [ "$VERSION" == "" ]; then
 VERSION="1.0.1"
 fi
 
-start $VERSION
+start $M2_REPOSITORY $VERSION
 
 exit 1
 
