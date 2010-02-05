21c3e1ac2f0989688f06c49911e2ab285ec9c322
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2010-02-05 06:59:34 +0000

    a mountain of work to do :-)
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5580 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/jsr330-tck/src/test/MyTck.java
+++ pico/jsr330-tck/src/test/MyTck.java
@@ -1,10 +1,14 @@
 import junit.framework.Test;
 import org.atinject.tck.Tck;
 import org.atinject.tck.auto.Car;
+import org.atinject.tck.auto.Convertible;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.MutablePicoContainer;
 
 public class MyTck {
     public static Test suite() {
-        Car car = null; // new MyInjector().getInstance(Car.class);
+        MutablePicoContainer dpc = new DefaultPicoContainer().addComponent(Car.class, Convertible.class);
+        Car car = dpc.getComponent(Car.class);
         return Tck.testsFor(car, true, true);
     }
 }
