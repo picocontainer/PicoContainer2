f6b84a57d5679ceb684310bf431e20913fc4c201
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2010-02-07 01:32:29 +0000

    more on simulation
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5582 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/jsr330-tck/src/test/StoneageSimulationJsr330Tests.java
+++ pico/jsr330-tck/src/test/StoneageSimulationJsr330Tests.java
@@ -2,21 +2,28 @@ import junit.framework.Test;
 import org.atinject.tck.Tck;
 import org.atinject.tck.auto.Car;
 import org.atinject.tck.auto.Convertible;
+import org.atinject.tck.auto.DriversSeat;
+import org.atinject.tck.auto.Engine;
 import org.atinject.tck.auto.FuelTank;
+import org.atinject.tck.auto.GasEngine;
 import org.atinject.tck.auto.Seat;
 import org.atinject.tck.auto.Tire;
+import org.atinject.tck.auto.V8Engine;
 import org.atinject.tck.auto.accessories.Cupholder;
 import org.atinject.tck.auto.accessories.SpareTire;
 import org.picocontainer.DefaultPicoContainer;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class StoneageSimulationJsr330Tests {
-    public static Test suite() throws InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
+    public static Test suite() throws InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException, NoSuchFieldException {
         Constructor<?> seatCtor = Seat.class.getDeclaredConstructors()[0];
+        Constructor<?> driversSeatCtor = DriversSeat.class.getDeclaredConstructors()[0];
         seatCtor.setAccessible(true);
 
         final Seat[] plainSeat = new Seat[1];
@@ -29,7 +36,7 @@ public class StoneageSimulationJsr330Tests {
 
         Cupholder cupholder = new Cupholder(plainSeatProvider);
         plainSeat[0] = (Seat) seatCtor.newInstance(cupholder);
-        final Seat driversSeat = (Seat) seatCtor.newInstance(cupholder);
+        final DriversSeat driversSeat = (DriversSeat) driversSeatCtor.newInstance(cupholder);
 
         FuelTank fuelTank = new FuelTank();
 
@@ -55,11 +62,17 @@ public class StoneageSimulationJsr330Tests {
             }
         };
 
+        Provider<Engine> engineProvider = new Provider<Engine>() {
+            public Engine get() {
+                return new V8Engine();
+            }
+        };
+
         Constructor<?> convertibelCtor = null;
         Constructor<?>[] convertibelCtors = Convertible.class.getDeclaredConstructors();
         for (int i = 0; i < convertibelCtors.length; i++) {
             convertibelCtor = convertibelCtors[i];
-            if (convertibelCtor.getParameterTypes().length >0) {
+            if (convertibelCtor.getParameterTypes().length > 0) {
                 break;
             }
         }
@@ -74,7 +87,28 @@ public class StoneageSimulationJsr330Tests {
                 plainSeatProvider,
                 driversSeatProvider,
                 plainTireProvider,
-                spareTireProvider);
+                engineProvider);
+
+
+        Field driversSeatAFld = Convertible.class.getDeclaredField("driversSeatA");
+        driversSeatAFld.setAccessible(true);
+        driversSeatAFld.set(car, driversSeat);
+
+        Field driversSeatBFld = Convertible.class.getDeclaredField("driversSeatB");
+        driversSeatBFld.setAccessible(true);
+        driversSeatBFld.set(car, driversSeat);
+
+        Field spareTireFld = Convertible.class.getDeclaredField("spareTire");
+        spareTireFld.setAccessible(true);
+        spareTireFld.set(car, spareTire);
+
+        Field cupholderFld = Convertible.class.getDeclaredField("cupholder");
+        cupholderFld.setAccessible(true);
+        cupholderFld.set(car, cupholder);
+
+        Field engineProviderFld = Convertible.class.getDeclaredField("engineProvider");
+        engineProviderFld.setAccessible(true);
+        engineProviderFld.set(car, engineProvider);
 
         return Tck.testsFor(car, true, true);
     }
