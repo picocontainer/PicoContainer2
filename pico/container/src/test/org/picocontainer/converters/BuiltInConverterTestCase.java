f1bf9d0976ed11d12766f8df15032bd2a02e9f58
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2010-02-21 09:30:10 +0000

    Refactored test case to reduce duplication and be more readable.
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5628 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/container/src/test/org/picocontainer/converters/BuiltInConverterTestCase.java
+++ pico/container/src/test/org/picocontainer/converters/BuiltInConverterTestCase.java
@@ -12,58 +12,74 @@ import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class BuiltInConverterTestCase {
+    BuiltInConverters bic = new BuiltInConverters();
 
     @Test
-    public void testBuiltInCanConvert() {
-        BuiltInConverters bic = new BuiltInConverters();
+    public void canConvertAllPrimitiveTypes() {
         assertTrue(bic.canConvert(Integer.class));
+        assertTrue(bic.canConvert(Integer.TYPE));
         assertTrue(bic.canConvert(Long.class));
+        assertTrue(bic.canConvert(Long.TYPE));
         assertTrue(bic.canConvert(Byte.class));
+        assertTrue(bic.canConvert(Byte.TYPE));
         assertTrue(bic.canConvert(Double.class));
+        assertTrue(bic.canConvert(Double.TYPE));
         assertTrue(bic.canConvert(Float.class));
-        assertTrue(bic.canConvert(File.class));
+        assertTrue(bic.canConvert(Float.TYPE));
         assertTrue(bic.canConvert(Character.class));
+        assertTrue(bic.canConvert(Character.TYPE));
         assertTrue(bic.canConvert(Short.class));
+        assertTrue(bic.canConvert(Short.TYPE));
         assertTrue(bic.canConvert(Boolean.class));
-        assertTrue(bic.canConvert(URL.class));
-        assertFalse(bic.canConvert(JPanel.class));
-    }
+        assertTrue(bic.canConvert(Boolean.TYPE));
 
-    @Test
-    public void testBuiltInConvert() throws MalformedURLException {
-        BuiltInConverters bic = new BuiltInConverters();
         assertEquals(12, bic.convert("12", Integer.class));
+        assertEquals(12, bic.convert("12", Integer.TYPE));
+        assertEquals(12345678901L, bic.convert("12345678901",Long.TYPE));
         assertEquals(12345678901L, bic.convert("12345678901",Long.class));
         assertEquals((byte)12, bic.convert("12", Byte.class));
+        assertEquals((byte)12, bic.convert("12", Byte.TYPE));
         assertEquals(2.22, bic.convert("2.22", Double.class));
+        assertEquals(2.22, bic.convert("2.22", Double.TYPE));
         assertEquals(1.11F, bic.convert("1.11", Float.class));
-        assertEquals(new File("c:\\foo"), bic.convert("c:\\foo", File.class));
+        assertEquals(1.11F, bic.convert("1.11", Float.TYPE));
         assertEquals('a', bic.convert("a", Character.class));
+        assertEquals('a', bic.convert("a", Character.TYPE));
         assertEquals((short)12, bic.convert("12", Short.class));
+        assertEquals((short)12, bic.convert("12", Short.TYPE));
         assertEquals(Boolean.TRUE, bic.convert("TRUE", Boolean.class));
+        assertEquals(true, bic.convert("TRUE", Boolean.TYPE));
+    }
+
+    
+    @Test
+    public void canConvertFileAndURL() throws MalformedURLException {
+        assertTrue(bic.canConvert(File.class));
+        assertTrue(bic.canConvert(URL.class));
+        assertEquals(new File("c:\\foo"), bic.convert("c:\\foo", File.class));
         assertEquals(new URL("http://example.com"), bic.convert("http://example.com", URL.class));
-        assertEquals(null, bic.convert("anything", JPanel.class));
     }
 
     @Test
-    public void canAddAConverter() {
-        BuiltInConverters bic = new BuiltInConverters();
+    public void canAddCustomConverter() {
         bic.addConverter(new JPanelConverter(), JPanel.class);
         assertTrue(bic.convert("anything", JPanel.class) instanceof JPanel);
-
     }
 
+    @SuppressWarnings("serial")
     @Test
-    public void canSupplementConverters() {
-        BuiltInConverters bic = new BuiltInConverters() {
+    public void canSupplementBuiltInConverters() {
+        assertFalse(bic.canConvert(JPanel.class));
+
+        BuiltInConverters bicWithJPanel = new BuiltInConverters() {
             @Override
             protected void addBuiltInConverters() {
                 super.addBuiltInConverters();
                 super.addConverter(new JPanelConverter(), JPanel.class);
             }
         };
-        assertEquals(Boolean.TRUE, bic.convert("TRUE", Boolean.class));
-        assertTrue(bic.convert("anything", JPanel.class) instanceof JPanel);
+        assertEquals(Boolean.TRUE, bicWithJPanel.convert("TRUE", Boolean.class));
+        assertTrue(bicWithJPanel.convert("anything", JPanel.class) instanceof JPanel);
 
     }
 
@@ -74,25 +90,4 @@ public class BuiltInConverterTestCase {
     }
     
     
-    @Test
-    public void testPrimitivesAreSupported() {
-        BuiltInConverters converters = new BuiltInConverters();
-        assertTrue(converters.canConvert(Boolean.TYPE));
-        assertTrue(converters.canConvert(Character.TYPE));
-        assertTrue(converters.canConvert(Integer.TYPE));
-        assertTrue(converters.canConvert(Long.TYPE));
-        assertTrue(converters.canConvert(Short.TYPE));
-        assertTrue(converters.canConvert(Float.TYPE));
-        assertTrue(converters.canConvert(Double.TYPE));
-        assertTrue(converters.canConvert(Byte.TYPE));
-        
-        assertEquals(12, converters.convert("12", Integer.TYPE));
-        assertEquals(12345678901L, converters.convert("12345678901",Long.TYPE));
-        assertEquals((byte)12, converters.convert("12", Byte.TYPE));
-        assertEquals(2.22, converters.convert("2.22", Double.TYPE));
-        assertEquals(1.11F, converters.convert("1.11", Float.TYPE));
-        assertEquals('a', converters.convert("a", Character.TYPE));
-        assertEquals((short)12, converters.convert("12", Short.TYPE));
-        assertEquals(true, converters.convert("TRUE", Boolean.TYPE));
-    }
 }
