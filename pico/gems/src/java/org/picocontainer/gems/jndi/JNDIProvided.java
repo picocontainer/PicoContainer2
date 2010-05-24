96b35b96afa63aa77657d2e8d13720a23f05bc17
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2010-05-24 13:25:45 +0000

    JNDI fixup as per PICO-370
    git-svn-id: http://svn.codehaus.org/picocontainer/java/2.x/trunk@5681 ac66bb80-72f5-0310-8d68-9f556cfffb23

--- pico/gems/src/java/org/picocontainer/gems/jndi/JNDIProvided.java
+++ pico/gems/src/java/org/picocontainer/gems/jndi/JNDIProvided.java
@@ -20,49 +20,52 @@ import org.picocontainer.PicoContainer;
 import org.picocontainer.PicoVisitor;
 
 /**
- * represents dependency provided via JNDI. This dependency is not 
+ * Represents dependency provided via JNDI. This dependency is not 
  * to be managed by container at all, so there is no lifecycle, no 
  * monitoring etc. 
  * @author Konstantin Pribluda
  *
  */
 @SuppressWarnings("serial")
-public class JNDIProvided<T> implements ComponentAdapter<T> , Serializable {
+public class JNDIProvided<T> implements ComponentAdapter<T>, Serializable {
 
-
-	JNDIObjectReference<T> jndiReference;
-	
-	 Object componentKey;
+	private JNDIObjectReference<T> jndiReference;	
+	private Class<T> type;
+    private Object componentKey;
 	
 	/**
-	 * create adapter with specified key and reference
+	 * Create adapter with specified key and reference
 	 * @param componentKey component key
 	 * @param reference JNDI reference storing component
+     * @param type the type that the JNDIObjectReference will return.
 	 */
-	public JNDIProvided(final Object componentKey,final JNDIObjectReference<T> reference) {
+	public JNDIProvided(final Object componentKey,final JNDIObjectReference<T> reference, final Class<T> type) {
 		this.componentKey = componentKey;
 		this.jndiReference = reference;
+		this.type = type;
 	}
 	
 	/**
-	 * create adapter with JNDI reference. referenced object class will be 
+	 * Create adapter with JNDI reference. referenced object class will be
 	 * takes as key
 	 * @param reference JNDI reference storing component
+     * @param type the type that the JNDIObjectReference will return.
 	 */
-	public JNDIProvided(final JNDIObjectReference<T> reference) {
-		this(reference.get().getClass(),reference);
+	public JNDIProvided(final JNDIObjectReference<T> reference, Class<T> type) {
+		this(reference.get().getClass(),reference, type);
 	}
 	
 	/**
-	 * create adapter based on JNDI name. I leave this unchecked because
+	 * Create adapter based on JNDI name. I leave this unchecked because
 	 * type is really not known at this time
 	 * @param jndiName name to be used
-	 * @throws NamingException will be thrown if something goes 
+     * @param type the type that the JNDIObjectReference will return.
+     * @throws NamingException will be thrown if something goes
 	 * wrong in JNDI
 	 */
 	@SuppressWarnings("unchecked")
-	public JNDIProvided(final String jndiName) throws NamingException {
-		this(new JNDIObjectReference(jndiName));
+	public JNDIProvided(final String jndiName, Class<T> type) throws NamingException {
+		this(new JNDIObjectReference(jndiName), type);
 	}
 	
 	public Object getComponentKey() {	
@@ -71,7 +74,7 @@ public class JNDIProvided<T> implements ComponentAdapter<T> , Serializable {
 
 	@SuppressWarnings("unchecked")
 	public Class<? extends T> getComponentImplementation() {
-		return (Class<? extends T>) jndiReference.get().getClass();
+		return type;
 	}
 
     public T getComponentInstance(final PicoContainer container) throws PicoCompositionException {
@@ -79,7 +82,7 @@ public class JNDIProvided<T> implements ComponentAdapter<T> , Serializable {
     }
 
     /**
-	 * retrieve instance out of JNDI
+	 * Retrieve instance out of JNDI
 	 */
 	public T getComponentInstance(final PicoContainer container, final Type into)
 			throws PicoCompositionException {
@@ -88,12 +91,12 @@ public class JNDIProvided<T> implements ComponentAdapter<T> , Serializable {
 
 	/**
 	 * we have nothing to verify here
-	 */
+     */
 	public void verify(final PicoContainer container) throws PicoCompositionException {
 	}
 
 	/**
-	 * as there is no puprose of proceeding further down, 
+	 * As there is no puprose of proceeding further down,
 	 * we do nothing here
 	 */
 	public void accept(final PicoVisitor visitor) {
